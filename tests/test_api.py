1e360b0d34e337fec0419b16a6328f7e32cce392
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-14 17:38:50 -0600

    Update tests.test_oauth.TestOAuth class

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,6 @@
 import unittest
-from unittest.mock import patch
-from twitch.api.oauth import OAuth, authenticate
+from unittest.mock import Mock, patch
+from twitch.api.oauth import authenticate
 
 
 class TestOAuth(unittest.TestCase):
@@ -8,11 +8,19 @@ class TestOAuth(unittest.TestCase):
         self.mock_get_patch = patch("twitch.api.oauth.requests.get")
         self.mock_get = self.mock_get_patch.start()
 
-    def test_authenticate(self):
-        self.mock_get.return_value.ok = True
+        self.client_id = "aabbcc"
+        self.client_secret = "ddeeff"
+        self.access_token = "xxyyzz"
 
-        response = authenticate("aabbccdd", "eeffgghh")
-        self.assertIsNotNone(response)
+    def test_authenticate(self):
+        self.mock_get.return_value = Mock(ok=True)
+        self.mock_get.return_value.json.return_value = {"access_token":
+                                                        self.access_token}
+
+        oauth = authenticate(self.client_id, self.client_secret)
+
+        self.assertIsNotNone(oauth)
+        self.assertEqual(oauth.access_token, self.access_token)
 
     def tearDown(self):
         self.mock_get_patch.stop()

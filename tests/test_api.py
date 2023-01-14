c46e3921526df9da8da46a4da450805050b74136
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-14 17:43:12 -0600

    Add oauth module (#1)
    * Add api.oauth module
    
    * Add api.oauth.OAuth class
    
    * Add api.oauth.authenticate function
    
    * Add doc strings to api.oauth module
    
    * Add tests.test_api module
    
    * Add tests.test_api.TestOAuth class
    
    * Update tests.test_oauth.TestOAuth class

---
+++ tests/test_api.py
@@ -0,0 +1,26 @@
+import unittest
+from unittest.mock import Mock, patch
+from twitch.api.oauth import authenticate
+
+
+class TestOAuth(unittest.TestCase):
+    def setUp(self):
+        self.mock_get_patch = patch("twitch.api.oauth.requests.get")
+        self.mock_get = self.mock_get_patch.start()
+
+        self.client_id = "aabbcc"
+        self.client_secret = "ddeeff"
+        self.access_token = "xxyyzz"
+
+    def test_authenticate(self):
+        self.mock_get.return_value = Mock(ok=True)
+        self.mock_get.return_value.json.return_value = {"access_token":
+                                                        self.access_token}
+
+        oauth = authenticate(self.client_id, self.client_secret)
+
+        self.assertIsNotNone(oauth)
+        self.assertEqual(oauth.access_token, self.access_token)
+
+    def tearDown(self):
+        self.mock_get_patch.stop()
---
+++ twitch/api/oauth.py
@@ -0,0 +1,31 @@
+import requests
+from typing import NamedTuple
+
+
+class OAuth(NamedTuple):
+    """
+    OAuth class holds immutable fields needed
+    accessing Twitch API.
+    """
+
+    client_id: str
+    client_secret: str
+    access_token: str
+
+
+def authenticate(client_id: str, client_secret: str) -> OAuth:
+    """
+    Authenticates with Twitch service and returns
+    credentials in an instance of the OAuth class.
+    """
+
+    url = "https://id.twitch.tv/oauth2/token"
+    data = {"client_id": client_id,
+            "client_secret": client_secret,
+            "grant_type": "client_credentials"}
+
+    response = requests.get(url, data)
+    oauth = OAuth(client_id, client_secret,
+                  response.json()["access_token"])
+
+    return oauth

73fd900842b07585f51a646dfbf7e8125a11ba7e
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-14 15:38:14 -0600

    Add api.oauth.authenticate function

--- twitch/api/oauth.py
+++ twitch/api/oauth.py
@@ -6,3 +6,16 @@ class OAuth(NamedTuple):
     client_id: str
     client_secret: str
     access_token: str
+
+
+def authenticate(client_id: str, client_secret: str) -> OAuth:
+    url = "https://id.twitch.tv/oauth2/token"
+    data = {"client_id": client_id,
+            "client_secret": client_secret,
+            "grant_type": "client_credentials"}
+
+    response = requests.get(url, data)
+    oauth = OAuth(client_id, client_secret,
+                  response.json()["access_token"])
+
+    return oauth

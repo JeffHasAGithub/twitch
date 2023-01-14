0afff5f086ef15faeedfb04091644aca23f82838
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-14 16:17:22 -0600

    Add tests.test_api.TestOAuth class

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,18 @@
 import unittest
 from unittest.mock import patch
 from twitch.api.oauth import OAuth, authenticate
+
+
+class TestOAuth(unittest.TestCase):
+    def setUp(self):
+        self.mock_get_patch = patch("twitch.api.oauth.requests.get")
+        self.mock_get = self.mock_get_patch.start()
+
+    def test_authenticate(self):
+        self.mock_get.return_value.ok = True
+
+        response = authenticate("aabbccdd", "eeffgghh")
+        self.assertIsNotNone(response)
+
+    def tearDown(self):
+        self.mock_get_patch.stop()

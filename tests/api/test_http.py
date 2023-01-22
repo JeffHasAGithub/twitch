5cf7d49c788c1d1f87e96ac0c8ee6088c299ec56
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-22 16:47:35 -0600

    Add http tests (#4)
    * Add tests.test_api package
    
    * Add tests.test_api.test_http module
    
    * Add imports to tests.test_api.test_http module
    
    * Add tests.test_api.test_http.TestHttp class
    
    * Add test_get_valid method to tests.test_api.test_http.TestHttp class
    
    * Add test_post_valid method to tests.test_api.test_http.TestHttp class
    
    * Rename tests.test_api package to tests.api

---
+++ tests/api/test_http.py
@@ -0,0 +1,19 @@
+import unittest
+from unittest.mock import Mock, patch
+from twitch.api.http import get, post
+
+
+class TestHttp(unittest.TestCase):
+    @patch("twitch.api.http.requests.get")
+    def test_get_valid(self, mock_get):
+        mock_get.return_value = Mock(status_code=200)
+
+        response = get("https://test.com/test", {})
+        self.assertEqual(response.status_code, 200)
+
+    @patch("twitch.api.http.requests.post")
+    def test_post_valid(self, mock_get):
+        mock_get.return_value = Mock(status_code=200)
+
+        response = post("https://test.com/test", {})
+        self.assertEqual(response.status_code, 200)

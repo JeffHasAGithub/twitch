79fa3b469b0350438b50b69e06993600a2ea606f
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-22 13:24:26 -0600

    Add api.http.post function

--- twitch/api/http.py
+++ twitch/api/http.py
@@ -4,3 +4,8 @@ import requests
 def get(url: str, params: dict, **kwargs):
     response = requests.get(url, params, **kwargs)
     return response
+
+
+def post(url: str, params: dict, **kwargs):
+    response = requests.post(url, params, kwargs)
+    return response

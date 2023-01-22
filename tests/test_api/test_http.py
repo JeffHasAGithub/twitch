3baccedb70d16d14fe5fd7d2b789c0edd25bd661
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-22 16:02:48 -0600

    Add test_get_valid method to tests.test_api.test_http.TestHttp class

--- tests/test_api/test_http.py
+++ tests/test_api/test_http.py
@@ -4,4 +4,9 @@ from twitch.api.http import get, post
 
 
 class TestHttp(unittest.TestCase):
-    pass
+    @patch("twitch.api.http.requests.get")
+    def test_get_valid(self, mock_get):
+        mock_get.return_value = Mock(status_code=200)
+
+        response = get("https://test.com/test", {})
+        self.assertEqual(response.status_code, 200)

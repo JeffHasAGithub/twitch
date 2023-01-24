8ddd9193f6148e997d5526ac40c2ade3dc341368
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-23 18:46:42 -0600

    Update http module (#5)
    * Update test_get_valid method in tests.api.test_http.TestHttp class
    
    * Add extract_json function to twitch.api.http module
    
    * Revert test_get_valid method in tests.api.test_http.TestHttp class
    
    * Add test_extract_json_valid method to tests.api.test_http.TestHttp class
    
    * Add JsonError class to twitch.api.error module
    
    * Update extract_json function in twitch.api.http to raise JsonError
    
    * Add doc string to get function in twitch.api.http module
    
    * Add doc string to post function in twitch.api.http module
    
    * Add doc string to extract_json function in twitch.api.http module

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -1,6 +1,27 @@
 import unittest
 from unittest.mock import Mock, patch
-from twitch.api.http import get, post
+from twitch.api.http import get, post, extract_json
+
+_json = {"widget": {"debug": "on",
+                    "window": {"title": "Sample Konfabulator Widget",
+                               "name": "main_window",
+                               "width": 500,
+                               "height": 500},
+                    "image": {"src": "Images/Sun.png",
+                              "name": "sun1",
+                              "hOffset": 250,
+                              "vOffset": 250,
+                              "alignment": "center"},
+                    "text": {"data": "Click Here",
+                             "size": 36,
+                             "style": "bold",
+                             "name": "text1",
+                             "hOffset": 250,
+                             "vOffset": 100,
+                             "alignment": "center",
+                             "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"}
+                    }
+         }
 
 
 class TestHttp(unittest.TestCase):
@@ -17,3 +38,14 @@ class TestHttp(unittest.TestCase):
 
         response = post("https://test.com/test", {})
         self.assertEqual(response.status_code, 200)
+
+    @patch("twitch.api.http.requests.get")
+    def test_extract_json_valid(self, mock_get):
+        mock_get.return_value = Mock(status_code=200)
+        mock_get.return_value.json.return_value = _json
+
+        response = mock_get()
+        json = extract_json(response)
+
+        self.assertIsNotNone(json)
+        self.assertDictEqual(json, _json)
--- twitch/api/http.py
+++ twitch/api/http.py
@@ -1,10 +1,15 @@
 import requests
 import requests.exceptions
 
-from .error import HttpError
+from .error import HttpError, JsonError
 
 
 def get(url: str, params: dict, **kwargs):
+    """
+    Perform GET request and return response.
+    Raise HttpError on bad request.
+    """
+
     try:
         response = requests.get(url, params, **kwargs)
         response.raise_for_status()
@@ -15,6 +20,11 @@ def get(url: str, params: dict, **kwargs):
 
 
 def post(url: str, params: dict, **kwargs):
+    """
+    Perform POST request and return response.
+    Raise HttpError on bad request.
+    """
+
     try:
         response = requests.post(url, params, kwargs)
         response.raise_for_status()
@@ -22,3 +32,21 @@ def post(url: str, params: dict, **kwargs):
         raise HttpError(f"Http error: {e}")
 
     return response
+
+
+def extract_json(response: requests.Response, **kwargs):
+    """
+    Extract JSON from given instance of requests.Response class.
+    Raise JsonError on invalid JSON.
+
+    Return dict if JSON is valid, None otherwise.
+    """
+    if not response:
+        return None
+
+    try:
+        json = response.json(**kwargs)
+    except requests.exceptions.InvalidJSONError:
+        raise JsonError("Json error: invalid JSON")
+
+    return json

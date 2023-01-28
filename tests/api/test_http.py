828b70cc03a06c93f2a18e46843614f34f6a1caf
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-27 22:17:37 -0600

    Update http module (#8)
    * Move params argument in get, post funcs to kwargs argument
    
    * Update TestHttp class in tests.api.test_http
    
    * Catch ConnectionError in twitch.api.http.get function
    
    * Update HttpError msg when thrown from twitch.api.http.get function
    
    * Catch ConnectionError in twitch.api.http.post function
    
    * Update HttpError msg when thrown from twitch.api.http.post function
    
    * Add test_get_bad_request method to tests.api.test_http.TestHttp class
    
    * Add test_get_bad_connection method to tests.api.test_http.TestHttp class
    
    * Add test_post_bad_request method to tests.api.test_http.TestHttp class
    
    * Add test_post_bad_connection method to tests.api.test_http.TestHttp class

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -1,6 +1,8 @@
 import unittest
+import requests
 from unittest.mock import Mock, patch
 from twitch.api.http import get, post, extract_json
+from twitch.api.error import HttpError
 
 _json = {"widget": {"debug": "on",
                     "window": {"title": "Sample Konfabulator Widget",
@@ -29,15 +31,45 @@ class TestHttp(unittest.TestCase):
     def test_get_valid(self, mock_get):
         mock_get.return_value = Mock(status_code=200)
 
-        response = get("https://test.com/test", {})
+        response = get("https://test.com/test")
         self.assertEqual(response.status_code, 200)
+
+    @patch("twitch.api.http.requests.get")
+    def test_get_bad_request(self, mock_get):
+        mock_get.return_value = Mock(status_code=404)
+        mock_get.side_effect = requests.exceptions.HTTPError(response=mock_get)
+
+        with self.assertRaises(HttpError):
+            get("https://test.com/test")
+
+    @patch("twitch.api.http.requests.get")
+    def test_get_bad_connection(self, mock_get):
+        mock_get.side_effect = requests.exceptions.ConnectionError()
+
+        with self.assertRaises(HttpError):
+            get("https://test.com/test")
 
     @patch("twitch.api.http.requests.post")
     def test_post_valid(self, mock_get):
         mock_get.return_value = Mock(status_code=200)
 
-        response = post("https://test.com/test", {})
+        response = post("https://test.com/test")
         self.assertEqual(response.status_code, 200)
+
+    @patch("twitch.api.http.requests.post")
+    def test_post_bad_request(self, mock_post):
+        mock_post.return_value = Mock(status_code=404)
+        mock_post.side_effect = requests.exceptions.HTTPError(response=mock_post)
+
+        with self.assertRaises(HttpError):
+            post("https://test.com/test")
+
+    @patch("twitch.api.http.requests.post")
+    def test_post_bad_connection(self, mock_post):
+        mock_post.side_effect = requests.exceptions.ConnectionError()
+
+        with self.assertRaises(HttpError):
+            post("https://test.com/test")
 
     @patch("twitch.api.http.requests.get")
     def test_extract_json_valid(self, mock_get):
